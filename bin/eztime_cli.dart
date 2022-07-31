6b4dd77aefee7153611dbb3426b98b296f5e3eb8
Jaquie <jaq.cat@protonmail.ch>
2022-07-31 17:36:13 -0300

    yay

--- bin/eztime_cli.dart
+++ bin/eztime_cli.dart
@@ -1,27 +1,45 @@
+import 'dart:io';
+
 import 'package:eztime/eztime.dart';
 
-const help =
-    "eztime-cli\nEasily calculate time sums and differences\n  Usage: eztime <HH:MM:SS> [+, -] <HH:MM:SS>";
+const help = "eztime-cli - Easily calculate time sums and differences\n"
+    "Usage: eztime <time> [+, -] <time>\n\n"
+    "<time> can be in 2 formats: HH:MM or HH:MM:SS"
+    " second(s)]\n"
+    "A few examples are 12:00:00, 12 hours, 30 minutes, 55s and 12:30:55";
+
+Time? parse(String t) {
+  List<String> parts = t.split(':');
+  if (parts.length != 2 && parts.length != 3) return null;
+  final h = int.tryParse(parts[0]);
+  final m = int.tryParse(parts[1]);
+  double s = 0.0;
+  if (parts.length == 3) s = double.tryParse(parts[2]) ?? 0;
+  if (h == null || m == null) return null;
+  return Time(h, m, s);
+}
 
 void main(List<String> arguments) {
+  // check if help
   if (arguments.length != 3) {
     print(help);
     return;
   }
-
-  final a = Time.fromString(arguments.first);
-  final b = Time.fromString(arguments.last);
+  final a = parse(arguments.first);
+  final b = parse(arguments.last);
   if (a == null || b == null) {
-    print(help);
+    stderr.writeln(help);
     return;
   }
 
   switch (arguments[1]) {
     case '+':
       print(a + b);
-      break;
+      return;
     case '-':
       print(a - b);
-      break;
+      return;
+    default:
+      stderr.writeln(help);
   }
 }
--- test/eztime_cli_test.dart
+++ test/eztime_cli_test.dart
@@ -2,7 +2,4 @@ import 'package:eztime_cli/eztime_cli.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
 }
